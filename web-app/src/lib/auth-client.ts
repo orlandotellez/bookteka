d70a57e1df58ce7f32a7196e6f2be4b02f386de8
orlandotellez <espadachinkinno@gmail.com>
2026-03-15 19:04:08 -0600

    fix: cambiar de BASE_URL a VITE_BETTER_AUTH__CLIENT para el cliente de better auth

--- web-app/src/lib/auth-client.ts
+++ web-app/src/lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
-const API_URL = import.meta.env.BASE_URL
+const API_URL = import.meta.env.VITE_BETTER_AUTH_CLIENT
+
 
 export const authClient = createAuthClient({
   baseURL: API_URL,
