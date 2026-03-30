b5cad8ada764976f717870c63db147f38f7b62ef
orlandotellez <espadachinkinno@gmail.com>
2026-03-30 01:12:27 -0600

    feat: agrgar _layout principal de la app y con tipos para mantener confibilidad de stacks de rutas

---
+++ mobile-app/app/_layout.tsx
@@ -0,0 +1,41 @@
+import { Stack } from 'expo-router';
+
+type RootRoutes = "(tabs)";
+
+interface StackConfig {
+  name: RootRoutes;
+  headerShown: boolean;
+  title?: string;
+  presentation?: 'modal' | 'card' | 'fullScreenModal';
+}
+
+const ROOT_STACK: StackConfig[] = [
+  {
+    name: "(tabs)",
+    headerShown: false
+  },
+];
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: '#000' },
+        headerTintColor: '#fff',
+        contentStyle: { backgroundColor: '#000' },
+      }}
+    >
+      {ROOT_STACK.map((route) => (
+        <Stack.Screen
+          key={route.name}
+          name={route.name}
+          options={{
+            headerShown: route.headerShown,
+            title: route.title,
+            presentation: route.presentation,
+          }}
+        />
+      ))}
+    </Stack>
+  );
+}
